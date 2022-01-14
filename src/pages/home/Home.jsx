379c55af1ca954029c7c41c30d36470bcc1ea98f
Cornal <nguyendangduy2210@gmail.com>
2022-01-14 13:34:03 +0700

    initial

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Topbar from '../../components/topbar/Topbar';
+import './home.css';
+
+export default function Home() {
+    return(
+        <>
+            <Topbar />
+            <div className="homeContainer"></div>
+        </>
+    )
+}
